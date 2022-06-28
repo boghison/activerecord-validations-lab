ff5c418d35531119dcadad95f907466825c3332c
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-28 18:48:39 +0300

    commit tree f1bb5c258d2a66366911353c55f329bf8de9001e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :is_clickbaity
+
+    def is_clickbaity
+        if title.nil? || !(title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top "))
+            errors.add(:title, "not clickbait")
+        end
+    end
 end
